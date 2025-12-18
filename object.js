2c68dc965279ab9f061aa1e01df59cca6edfc091
neelabangari <neelabangari21@gmail.com>
2025-12-18 08:23:56 +0530

    commited on 18

---
+++ object.js
@@ -0,0 +1,9 @@
+const ob={
+    "name": "vaish",
+    "age": 23
+};
+console.log(ob.name);
+console.log("all porop");
+for (let k in ob){
+    console.log(k+":"+ob[k]);
+}
---
+++ product.js
@@ -0,0 +1,9 @@
+const ob={
+    "name": "vaish",
+    "age": 23
+};
+console.log(ob);
+ob.name="varshitha";
+console.log(ob);
+delete ob.age;
+console.log(ob);
